109318850c2972977f77fb987fab704795b36512
Omar <omar@fevrok.com>
2020-09-22 00:43:21 +0100

    Fetch all products from the database

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -13,17 +13,8 @@ class ProductsController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        $products = Product::get();
+        return response()->json($products, 200);
     }
 
     /**
