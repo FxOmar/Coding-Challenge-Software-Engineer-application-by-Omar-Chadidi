cb1f2be3a904677b6388ff763d517d27ab42b90c
Omar <omar@fevrok.com>
2020-09-23 05:13:06 +0100

    CRUD 🏢

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class CategoriesController extends Controller
 {
@@ -13,7 +14,9 @@ class CategoriesController extends Controller
      */
     public function index()
     {
-        //
+        $categories = Category::get();
+        return response()->json($categories, 200);
+        
     }
 
     /**
@@ -24,7 +27,12 @@ class CategoriesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $category = new Category();
+        $category->name = $request->name;
+
+        $category->save();
+
+        return response()->json(['message' => 'Category created successfully.'], 200);
     }
 
     /**
@@ -35,7 +43,16 @@ class CategoriesController extends Controller
      */
     public function show($id)
     {
-        //
+        if (Category::where('id', $id)->exists()) {
+
+            $category = Category::find($id);
+
+            return response()->json($category, 200);
+
+        } else {
+            return response()->json(['message' => "Category not found!"], 404);
+        }
+
     }
 
     /**
@@ -47,7 +64,23 @@ class CategoriesController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        if (Category::where('id', $id)->exists()) {
+
+            $category = Category::find($id);
+
+            if ($request->has('name')) {
+                $category->name = $request->name;
+            } else {
+                $category->name = $category->name;
+            }
+
+            $category->save();
+
+            return response()->json(['message' => 'Category updated successfully.'], 200);
+
+        } else {
+            return response()->json(['message' => "Category not found!"], 404);
+        }
     }
 
     /**
@@ -58,6 +91,16 @@ class CategoriesController extends Controller
      */
     public function destroy($id)
     {
-        //
+        if (Category::where('id', $id)->exists()) {
+
+            $category = Category::find($id);
+
+            $category->delete();
+
+            return response()->json(['message' => 'Category deleted successfully.'], 200);
+
+        } else {
+            return response()->json(['message' => "Category not found!"], 404);
+        }
     }
 }
